f2541bf5b26ed1d428ee6acd83dbe7c3f197e16a
Kayuzee <qayyum.rajan@gmail.com>
2020-01-25 18:29:04 -0800

    Add metamask option

--- communitydao/app/src/drizzleOptions.js
+++ communitydao/app/src/drizzleOptions.js
@@ -4,7 +4,7 @@ import CommunityDAO from "./contracts/CommunityDAO.json"
 const options = {
   web3: {
     block: false,
-    customProvider: new Web3("ws://localhost:8545"),
+  customProvider: new Web3("ws://localhost:8545"), /* use window.web3.currentProvider for metamask */
   },
   contracts: [CommunityDAO],
   events: {
